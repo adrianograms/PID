d08de66b11229e071f476cec6e383eae0e31e3af
Mulling <lucas_mulling@hotmail.com>
2020-10-22 14:57:53 -0300

    add CLI

--- main.py
+++ main.py
@@ -1,13 +1,109 @@
-import numpy as np
 from PIL import Image
-#joguei as funções para o arquivo morph.py
 import morph as mp
+import numpy as np
+import getopt
+import sys
 
-im = Image.open("mul.jpeg").convert("L")
 
-#Passa imagem e quantidade de interações
-image = mp.erode(im,1)
+def get_structuring_element(morph, size):
+    if morph == 'cross':
+        k = np.zeros((size, size), dtype=np.uint8)
+        k[:, int(size / 2)] = 1
+        k[int(size / 2), :] = 1
+        return k
 
-#convert back
-im = Image.fromarray(np.uint8(image))
-im.show()
+    return np.ones((size, size), dtype=np.uint8)
+
+
+def read_kernel(size):
+    k = np.empty((size, size), dtype=np.uint8)
+
+    # achar uma maneira melhor de fazer isso?
+    for i in range(size):
+        for j in range(size):
+            k[i][j] = 0 if int(input()) == 0 else 1
+
+    return k
+
+
+def main():
+    in_file_name = None
+    out_file_name = None
+    kernel = None
+    kernel_size = None
+    mopt = None                  # 0 = erode, 1 = dilate
+
+    iterations = 1
+
+    custom_kernel = False
+
+    mopts = [mp.erode, mp.dilate]
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'i:o:m:s:r:edc')
+        # -i nome do arquivo de entrada
+        # -o nome do arquivo de saida
+        # -m tipo do kernel
+        # -s tamanho de kernel (sempre quadrado)
+        # -r numero de iteracoes
+        # -e erode
+        # -d dilate
+        # -c ler kernel do user
+    except getopt.GetoptError as err:
+        print(err)
+        sys.exit(1)
+
+    for opt, arg in opts:
+        if opt == '-i':
+            in_file_name = arg
+        elif opt == '-o':
+            out_file_name = arg
+        elif opt == '-m':
+            kernel = arg
+        elif opt == '-s':
+            kernel_size = int(arg)
+        elif opt == '-c':
+            custom_kernel = True
+        elif opt == '-r':
+            iterations = int(arg)
+        elif opt == '-e':
+            mopt = 0
+        elif opt == '-d':
+            mopt = 1
+
+    if None in (in_file_name, out_file_name, kernel_size, opt) and not custom_kernel:
+        print('Error: Missing paramether')
+        sys.exit(1)
+
+    if kernel_size == 1 or kernel_size % 2 == 0:
+        print('Error: Kernel size must be odd and bigger than 1')
+        sys.exit(1)
+
+    if custom_kernel:
+        kernel = read_kernel(kernel_size)
+    elif kernel in ('cross', 'rectangle'):
+        kernel = get_structuring_element(kernel, kernel_size)
+    else:
+        print('Error: Invalid kernel paramether')
+
+    im = []
+    try:
+        im = Image.open(in_file_name).convert('L')
+
+    except:
+        print(f'Error: Fail to open image {in_file_name}')
+        sys.exit(1)
+
+    # Passa imagem e quantidade de interações
+    image = mopts[mopt](np.array(im, dtype=np.uint8), kernel, iterations)
+
+    # convert back
+    im = Image.fromarray(np.uint8(image))
+    im.save(out_file_name)
+    im.show()
+
+
+if __name__ == '__main__':
+    main()
+
+
--- morph.py
+++ morph.py
@@ -1,55 +1,53 @@
 import numpy as np
-from PIL import Image
-
-def erode(im, iterations = 1):
-    #Uma outra ideia para pegar o vetor de indices
-    k = np.ones((3,3),dtype=np.uint8).flatten()
-    kernel = [i for i in range(len(k)) if k[i]]
-
-    #convert to np array
-    image = np.array(im)
 
 
-    x = 1
-    y = 1
+def kernel_to_index(kernel):
+    fkernel = kernel.flatten()
+
+    return [i for i in range(len(fkernel)) if fkernel[i] != 0]
+
+
+def erode(image, kernel, iterations=1):
+    ikernel = kernel_to_index(kernel)
+
+    x = int(kernel.shape[0] / 2)
+    y = int(kernel.shape[1] / 2)
+
+    pad_image = np.zeros((image.shape[0] + x * 2, image.shape[1] + y * 2),
+                         dtype=np.uint8)
+
+    pad_image[:] = 255
+
     for _ in range(iterations):
-      pad_image = np.zeros((image.shape[0] + x * 2, image.shape[1] + y * 2), dtype=np.uint8)
+        pad_image[x:x + image.shape[0], y:y + image.shape[1]] = image
 
-      pad_image[:] = 255
-
-      pad_image[x:x + image.shape[0], y:y + image.shape[1]] = image
-
-    
-      for i in range(x,pad_image.shape[0]-x):
-        for j in range(y,pad_image.shape[1]-y):
-          aux = pad_image[i-x:i+x+1,j-y:j+y+1]
-          image[i-x,j-y] = np.amin(aux.flatten()[kernel])
+        for i in range(x, pad_image.shape[0] - x):
+            for j in range(y, pad_image.shape[1] - y):
+                image[i - x, j - y] = np.amin(pad_image[i - x:i + x + 1,
+                                                        j - y:j + y + 1]
+                                              .flatten()[ikernel])
 
     return image
 
 
-def dilate(im, iterations = 1):
-    #Uma outra ideia para pegar o vetor de indices
-    k = np.ones((3,3),dtype=np.uint8).flatten()
-    kernel = [i for i in range(0,len(k)) if k[i]]
+def dilate(image, kernel, iterations=1):
+    ikernel = kernel_to_index(kernel)
 
-    #convert to np array
-    image = np.array(im)
+    x = int(kernel.shape[0] / 2)
+    y = int(kernel.shape[1] / 2)
 
+    pad_image = np.zeros((image.shape[0] + x * 2, image.shape[1] + y * 2),
+                         dtype=np.uint8)
 
-    x = 1
-    y = 1
+    pad_image[:] = 0
+
     for _ in range(iterations):
-      pad_image = np.zeros((image.shape[0] + x * 2, image.shape[1] + y * 2), dtype=np.uint8)
+        pad_image[x:x + image.shape[0], y:y + image.shape[1]] = image
 
-      pad_image[:] = 0
-
-      pad_image[x:x + image.shape[0], y:y + image.shape[1]] = image
-
-      for i in range(x,pad_image.shape[0]-x):
-        for j in range(y,pad_image.shape[1]-y):
-          aux = pad_image[i-x:i+x+1,j-y:j+y+1]
-          image[i-x,j-y] = np.amax(aux.flatten()[kernel])
+        for i in range(x, pad_image.shape[0] - x):
+            for j in range(y, pad_image.shape[1] - y):
+                image[i - x, j - y] = np.amax(pad_image[i - x:i + x + 1,
+                                                        j - y:j + y + 1]
+                                              .flatten()[ikernel])
 
     return image
-
