e96bccd81ed8158d8f6ba9ec5437d6868a05ec85
vangolax <mateus.edival@gmail.com>
2020-12-12 23:06:51 -0300

    floodfill

--- 2/extract.py
+++ 2/extract.py
@@ -2,6 +2,41 @@ import numpy as np
 from PIL import Image
 
 
+def floodfill(image, y,x, bg, limit):
+
+  #limit = [minx, maxx, miny, maxy]
+  new_image = np.zeros((limit[1]-limit[0],limit[3]-limit[2],3), dtype=int)
+  new_image[:] = [255, 255, 255]
+  q = [(y,x)]
+  bg_color = np.array([255,255,255])
+  new_image[y - limit[2], x - limit[0]] = image[y,x]
+  image[y,x] = bg_color
+
+  while(len(q)):
+    pos = q[0]
+    q.pop(0)
+
+    if(image[pos[0],pos[1]+1].mean() < bg):
+      q.append((pos[0],pos[1]+1))
+      new_image[pos[0] - limit[2], pos[1]+1 - limit[0]] = image[pos[0],pos[1]+1]
+      image[pos[0],pos[1]+1] = bg_color
+
+    if(image[pos[0],pos[1]-1].mean() < bg):
+      q.append((pos[0],pos[1]-1))
+      new_image[pos[0] - limit[2], pos[1]-1 - limit[0]] = image[pos[0],pos[1]-1]
+      image[pos[0],pos[1]-1] = bg_color
+
+    if(image[pos[0]-1,pos[1]].mean() < bg):
+      q.append((pos[0]-1,pos[1]))
+      new_image[pos[0]-1 - limit[2], pos[1] - limit[0]] = image[pos[0]-1,pos[1]]
+      image[pos[0]-1,pos[1]] = bg_color
+
+    if(image[pos[0]+1,pos[1]].mean() < bg):
+      q.append((pos[0]+1,pos[1]))
+      new_image[pos[0]+1 - limit[2], pos[1] - limit[0]] =  image[pos[0]+1,pos[1]]
+      image[pos[0]+1,pos[1]] = bg_color
+  return new_image
+
 def foo(file):
     name_image = file[7:-4]
 
@@ -13,6 +48,7 @@ def foo(file):
     border = []
 
     image_copy = np.array(im)
+    
     image = np.array(im.convert("L"))
 
     width = image.shape[0]
@@ -49,19 +85,20 @@ def foo(file):
                             break
                         c = (c + 1)%8
                 a = np.array(border)
-                print(a)
                 miny = np.min(a[:,0]) - 1
-                maxy = np.max(a[:,0]) + 1
+                maxy = np.max(a[:,0]) + 50
                 minx = np.min(a[:,1]) - 1
-                maxx = np.max(a[:,1]) + 1
+                maxx = np.max(a[:,1]) + 50
+                limit = [minx, maxx, miny, maxy]
+                if(len(border) > 20):
+                    image_color = floodfill(image_copy, border[0][0],border[0][1], bg, limit)
+                    Image.fromarray(np.uint8(image_color)).save("Imagens/" + name_image + "_" + str(count) + ".png")
+                    count = count + 1
 
-                image_color = image_copy[miny : maxy,  minx : maxx]
-                Image.fromarray(np.uint8(image_color)).save("Imagens/" + name_image + "_" + str(count) + ".png")
-                count = count + 1
-                image[miny : maxy,  minx : maxx] = 255
+                if(count > 0):
+                    Image.fromarray(np.uint8(image_copy)).show()
+                    return
                 border = []
-                #print(border)
-                #Image.fromarray(np.uint8(image)).show()
 
-    Image.fromarray(np.uint8(image)).show()
+
     #im = Image.fromarray(np.uint8(image))
