d1d1c5179e1a7e03c0b6284d0c781c7efca30573
vangolax <mateus.edival@gmail.com>
2020-12-13 20:57:42 -0300

    Imagem do perimetro

--- 2/extract.py
+++ 2/extract.py
@@ -2,11 +2,26 @@ import numpy as np
 #import cv2 as cv
 from PIL import Image
 
+
+def bar(border, limit):
+    """Cria array que representa a subimagem do perimetro"""
+
+    #instancia o array
+    new_image = np.zeros((limit[3]-limit[2],limit[1]-limit[0]), dtype=int)
+
+    #preenche com branco
+    new_image[:] = 255
+
+    #para cada pixel da borda a posição correspondente do new_image é pintado
+    #de preto
+    for x in border:
+      new_image[x[0] - limit[2],x[1] - limit[0]] = 0
+    return new_image
+
 def check_limit(shape, i, j):
     if(i >= 0 and j >=0 and i < shape[0] and j < shape[1]):
         return True
     return False
-
 
 def floodfill(image, y,x, bg, limit, image_grayscale):
 
@@ -16,11 +31,7 @@ def floodfill(image, y,x, bg, limit, image_grayscale):
   new_image = np.zeros((limit[3]-limit[2],limit[1]-limit[0],3), dtype=int)
   new_image[:] = [255, 255, 255]
   q = [(y,x)]
-  #bg = 220
-  #bg_color = np.array([255,255,255])
-  bg_color = [255,255,255]
   new_image[y - limit[2], x - limit[0]] = image[y,x]
-  image[y,x] = bg_color
   image_grayscale[y,x] = 255
 
   while(len(q)):
@@ -30,49 +41,41 @@ def floodfill(image, y,x, bg, limit, image_grayscale):
     if(image_grayscale[pos[0],pos[1]+1] < bg and check_limit(image_grayscale.shape, pos[0], pos[1] + 1)):
       q.append((pos[0],pos[1]+1))
       new_image[pos[0] - limit[2], pos[1]+1 - limit[0]] = image[pos[0],pos[1]+1]
-      #image[pos[0],pos[1]+1] = bg_color
       image_grayscale[pos[0],pos[1]+1] = 255
 
     if(image_grayscale[pos[0],pos[1]-1] < bg and check_limit(image_grayscale.shape, pos[0], pos[1] - 1)):
       q.append((pos[0],pos[1]-1))
       new_image[pos[0] - limit[2], pos[1]-1 - limit[0]] = image[pos[0],pos[1]-1]
-      #image[pos[0],pos[1]-1] = bg_color
       image_grayscale[pos[0],pos[1]-1] = 255
 
     if(image_grayscale[pos[0]-1,pos[1]] < bg and check_limit(image_grayscale.shape, pos[0] - 1, pos[1])):
       q.append((pos[0]-1,pos[1]))
       new_image[pos[0]-1 - limit[2], pos[1] - limit[0]] = image[pos[0]-1,pos[1]]
-      #image[pos[0]-1,pos[1]] = bg_color
       image_grayscale[pos[0]-1,pos[1]] = 255
 
     if(image_grayscale[pos[0]+1,pos[1]] < bg and check_limit(image_grayscale.shape, pos[0] + 1, pos[1])):
       q.append((pos[0]+1,pos[1]))
       new_image[pos[0]+1 - limit[2], pos[1] - limit[0]] =  image[pos[0]+1,pos[1]]
-      #image[pos[0]+1,pos[1]] = bg_color
       image_grayscale[pos[0]+1,pos[1]] = 255
 
     if(image_grayscale[pos[0]+1,pos[1]+1] < bg and check_limit(image_grayscale.shape, pos[0] + 1, pos[1] + 1)):
       q.append((pos[0]+1,pos[1]+1))
       new_image[pos[0]+1 - limit[2], pos[1]+1 - limit[0]] = image[pos[0]+1,pos[1]+1]
-      #image[pos[0]+1,pos[1]+1] = bg_color
       image_grayscale[pos[0]+1,pos[1]+1] = 255
 
     if(image_grayscale[pos[0]-1,pos[1]-1] < bg and check_limit(image_grayscale.shape, pos[0] - 1, pos[1] - 1)):
       q.append((pos[0]-1,pos[1]-1))
       new_image[pos[0]-1 - limit[2], pos[1]-1 - limit[0]] = image[pos[0]-1,pos[1]-1]
-      #image[pos[0]-1,pos[1]-1] = bg_color
       image_grayscale[pos[0]-1,pos[1]-1] = 255
 
     if(image_grayscale[pos[0]-1,pos[1]+1] < bg and check_limit(image_grayscale.shape, pos[0] - 1, pos[1] + 1)):
       q.append((pos[0]-1,pos[1]+1))
       new_image[pos[0]-1 - limit[2], pos[1]+1 - limit[0]] = image[pos[0]-1,pos[1]+1]
-      #image[pos[0]-1,pos[1]+1] = bg_color
       image_grayscale[pos[0]-1,pos[1]+1] = 255
-      
+
     if(image_grayscale[pos[0]+1,pos[1]-1] < bg and check_limit(image_grayscale.shape, pos[0] + 1, pos[1] - 1)):
       q.append((pos[0]+1,pos[1]-1))
       new_image[pos[0]+1 - limit[2], pos[1]-1 - limit[0]] =  image[pos[0]+1,pos[1]-1]
-      #image[pos[0]+1,pos[1]-1] = bg_color
       image_grayscale[pos[0]+1,pos[1]-1] = 255
   return new_image
 
@@ -87,7 +90,7 @@ def foo(file):
     border = []
 
     image_copy = np.array(im)
-    
+
     image = np.array(im.convert("L"))
 
     height = image.shape[0]
@@ -139,12 +142,16 @@ def foo(file):
                 maxx = np.max(a[:,1]) + 1
                 limit = [minx, maxx, miny, maxy]
 
-                if(len(border) > 10):
+                if(len(border) > 20):
                     image_color = floodfill(image_copy, border[0][0],border[0][1], bg, limit, image)
-                    Image.fromarray(np.uint8(image_color)).save("Imagens/" + name_image + "_" + str(count) + ".png")
+                    image_p = bar(border,limit)
+                    name_file = "Imagens/" + name_image + "_" + str(count) + ".png"
+                    name_filep = "Imagens/" + name_image + "_" + str(count)+ '-P' + ".png"
+                    Image.fromarray(np.uint8(image_color)).save(name_file)
+                    Image.fromarray(np.uint8(image_p)).save(name_filep)
                     count = count + 1
 
                 border = []
 
-    Image.fromarray(np.uint8(image_copy)).show()
+    #Image.fromarray(np.uint8(image_copy)).show()
     #im = Image.fromarray(np.uint8(image))
